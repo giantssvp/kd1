4e823421d6ff29772c6771f6c1b8b8f74fefb30a
vitthal <vitthalpadwal89@gmail.com>
2018-06-11 21:02:30 +0530

    new design

---
+++ kd/kd/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace kd
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery.js",
+                        "~/Scripts/jquery.wizard.js",
+                        "~/Scripts/jquery.min.js",
+                        "~/Scripts/jquery.ui.custom.js",
+                        "~/Scripts/jquery.flot.min.js",
+                        "~/Scripts/jquery.flot.pie.min.js",
+                        "~/Scripts/jquery.flot.resize.min.js",
+                        "~/Scripts/fullcalendar.min.js",
+                        "~/Scripts/jquery.gritter.min.js",
+                        "~/Scripts/matrix.chat.js",
+                        "~/Scripts/jquery.uniform.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/bootstrap-responsive.min.css",
+                      "~/Content/fullcalendar.css",
+                      "~/Content/matrix-style.css",
+                      "~/Content/matrix-media.css",
+                      "~/font/font-awesome.css",
+                      "~/Content/jquery.gritter.css"));
+        }
+    }
+}
---
+++ kd/kd/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sites()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Executive()
+        {
+
+            return View();
+        }
+
+        public ActionResult Franchies()
+        {
+
+            return View();
+        }
+
+        public ActionResult Customer()
+        {
+
+            return View();
+        }
+        public ActionResult Booking()
+        {
+
+            return View();
+        }
+        public ActionResult PaymentCommit()
+        {
+
+            return View();
+        }
+        public ActionResult PaymentDetails()
+        {
+
+            return View();
+        }
+        public ActionResult Aggrement()
+        {
+
+            return View();
+        }
+        public ActionResult Finance()
+        {
+
+            return View();
+        }
+        public ActionResult FileStatus()
+        {
+
+            return View();
+        }
+        public ActionResult Report()
+        {
+
+            return View();
+        }
+
+    }
+}
